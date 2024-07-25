4603598ec0d1bb0bc33a4d72cea835248b63524a
francogcuenca <francogcuenca@gmail.com>
2024-07-25 15:48:53 -0300

    detaie

--- Semana4/Ejercicios_tipo_parcial/Tipo_parcial_3(entregable).py
+++ Semana4/Ejercicios_tipo_parcial/Tipo_parcial_3(entregable).py
@@ -12,6 +12,12 @@
 # d)Informar el tipo de alimento más caro.
 # f)El promedio de precio por kilo en total.
 
+
+# Constantes
+PESO_MAX = 1000
+PESO_MIN = 10
+
+
 # Flags
 flag_compra = True
 
@@ -31,7 +37,7 @@ print('\nBienvenido al cotizador de ingredientes para preparar comida al por may
 # Input y validaciones
 while flag_compra == True:
     peso = float(input('\nIngrese el peso del ingrediente en kilos (mayor a 10 y menor que 1000): '))
-    while peso<10 or peso>1000:
+    while peso<PESO_MIN or peso>PESO_MAX:
         peso = float(input('\n¡¡ERROR!! Peso no válido, reingrese el peso del ingrediente en kilos (mayor a 10 y menor que 1000): '))
 
     precio_kilo = float(input('\nIngrese el precio del ingrediente por kilo en número: $'))
@@ -66,18 +72,21 @@ while flag_compra == True:
         case "M":
             tipo_mas_caro = "mezcla"
 
+
 # Informe
-print('El importe bruto a pagar es de: $',str(round(acum_precio, 2)))
+acum_precio_str = round(acum_precio,2)
+print(f'El importe bruto a pagar es de: ${acum_precio_str}')
 
 if acum_peso>100 and acum_peso<300:
-    precio_con_descuento = acum_precio - acum_precio * 0.15
-    print('El importe total con descuento es de: $',str(round(precio_con_descuento, 2)))
+    precio_con_descuento = round(acum_precio - acum_precio * 0.15, 2)
+
+    print(f'El importe total con descuento es de: ${precio_con_descuento}')
 
 elif acum_peso>300:
-    precio_con_descuento = acum_precio - acum_precio * 0.25
-    print('El importe total con descuento es de: $',str(round(precio_con_descuento, 2)))
+    precio_con_descuento = round(acum_precio - acum_precio * 0.25, 2)
+    print(f'El importe total con descuento es de: ${precio_con_descuento}')
 
-print('El alimento más caro es del tipo',tipo_mas_caro)
+print(f'El alimento más caro es del tipo {tipo_mas_caro}')
 
 precio_promedio = round((acum_precio/acum_peso), 2)
-print('El promedio de precio por kilo total es de: $', str(precio_promedio))
+print(f'El promedio de precio por kilo total es de: ${precio_promedio}')
